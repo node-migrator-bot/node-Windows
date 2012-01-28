8e14542646cf443eb9da002fdb0ef2eeea0bf117
Brandon Benvie <brandon@bbenvie.com>
2012-01-27 22:22:38 -0500

    Reorganize structue. Clean up registry. Clean up driveAlias.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 module.exports = {
-	driveAlias: require('./driveAlias'),
-	getFontNames: require('./fonts').getNames,
-	associations: require('./associations').associations,
-	fileTypes: require('./associations').fileTypes,
-	registry: require('./registry'),
-	Command: require('./Command').Command,
-	execSync: require('./Command').execSync,
+  driveAlias:   require('./lib/driveAlias'),
+  runnable:     require('./lib/runnable'),
+  getFontNames: require('./lib/fonts').getNames,
+  associations: require('./lib/associations').associations,
+  fileTypes:    require('./lib/associations').fileTypes,
+  registry:     require('./lib/registry'),
+  windowsSDK:   require('./lib/winsdk.js'),
+  Command:      require('./lib/Command').Command,
+  execSync:     require('./lib/Command').execSync,
 };
